402bff9a474d3dacace9f5895a995f3e5414d3aa
AmmarDeh <121573402+AmmarDeh@users.noreply.github.com>
2024-01-21 22:46:21 -0700

    Expand 90.1 Air Conditioning data
    Expand and update 90.1 Air Conditioning data

--- building_energy_standards_data/database_tables/hvac_minimum_requirement_unitary_air_conditioners.py
+++ building_energy_standards_data/database_tables/hvac_minimum_requirement_unitary_air_conditioners.py
@@ -7,25 +7,27 @@ from building_energy_standards_data.database_engine.database_util import (
 RECORD_HELP = """
 Must provide a tuple that contains:
 template: TEXT
+equipment_type: TEXT
 cooling_type: TEXT
 heating_type: TEXT
 subcategory: TEXT
+application: TEXT
+electric_power_phase: NUMERIC
+region: TEXT
 minimum_capacity: NUMERIC
 maximum_capacity: NUMERIC
 start_date: TEXT
 end_date: TEXT
 minimum_seasonal_efficiency: NUMERIC
+minimum_seasonal_efficiency_2: NUMERIC
 minimum_energy_efficiency_ratio: NUMERIC
+minimum_energy_efficiency_ratio_2: NUMERIC
 minimum_integrated_part_load_value: NUMERIC
 minimum_integrated_energy_efficiency_ratio: NUMERIC
+minimum_combined_energy_efficiency_ratio: NUMERIC
 ptac_eer_coefficient_1: NUMERIC
 ptac_eer_coefficient_2: NUMERIC
-minimum_scop: NUMERIC
-cool_cap_ft: TEXT
-cool_cap_fflow: TEXT
-cool_eir_ft: TEXT
-cool_eir_fflow: TEXT
-cool_plf_fplr: TEXT
+off_mode_power: NUMERIC
 annotation: TEXT (optional)
 """
 
@@ -33,76 +35,82 @@ CREATE_HVAC_REQUIREMENT_UNITARY_AIR_CONDITIONERS_TABLE = """
 CREATE TABLE IF NOT EXISTS %s
 (id INTEGER PRIMARY KEY, 
 template TEXT NOT NULL, 
+equipment_type TEXT NOT NULL, 
 cooling_type TEXT NOT NULL,
-heating_type TEXT NOT NULL,
-subcategory TEXT NOT NULL,
+heating_type TEXT,
+subcategory TEXT,
+application TEXT,
+electric_power_phase NUMERIC,
+region TEXT,
 minimum_capacity NUMERIC,
 maximum_capacity NUMERIC,
 start_date TEXT,
 end_date TEXT,
 minimum_seasonal_efficiency NUMERIC,
+minimum_seasonal_efficiency_2 NUMERIC,
 minimum_energy_efficiency_ratio NUMERIC,
+minimum_energy_efficiency_ratio_2 NUMERIC,
 minimum_integrated_part_load_value NUMERIC,
 minimum_integrated_energy_efficiency_ratio NUMERIC,
+minimum_combined_energy_efficiency_ratio NUMERIC,
 ptac_eer_coefficient_1 NUMERIC,
 ptac_eer_coefficient_2 NUMERIC,
-minimum_scop NUMERIC,
-cool_cap_ft TEXT,
-cool_cap_fflow TEXT,
-cool_eir_ft TEXT,
-cool_eir_fflow TEXT,
-cool_plf_fplr TEXT,
+off_mode_power NUMERIC,
 annotation TEXT);
 """
 
 INSERT_A_UNITARY_AIR_CONDITIONERS_RECORD = """
     INSERT INTO %s (
 template, 
+equipment_type,
 cooling_type,
 heating_type,
 subcategory,
+application,
+electric_power_phase,
+region,
 minimum_capacity,
 maximum_capacity,
 start_date,
 end_date,
 minimum_seasonal_efficiency,
+minimum_seasonal_efficiency_2,
 minimum_energy_efficiency_ratio,
+minimum_energy_efficiency_ratio_2,
 minimum_integrated_part_load_value,
 minimum_integrated_energy_efficiency_ratio,
+minimum_combined_energy_efficiency_ratio,
 ptac_eer_coefficient_1,
 ptac_eer_coefficient_2,
-minimum_scop,
-cool_cap_ft,
-cool_cap_fflow,
-cool_eir_ft,
-cool_eir_fflow,
-cool_plf_fplr,
+off_mode_power,
 annotation
 ) 
-VALUES (?, ?, ?, ? ,? , ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
 """
 
 RECORD_TEMPLATE = {
     "template": "",
+    "equipment_type": "",
     "cooling_type": "",
     "heating_type": "",
     "subcategory": "",
+    "application": "",
+    "electric_power_phase": 0.0,
+    "region": "",
     "minimum_capacity": 0.0,
     "maximum_capacity": 0.0,
     "start_date": "",
     "end_date": "",
     "minimum_seasonal_efficiency": 0.0,
+    "minimum_seasonal_efficiency_2": 0.0,
     "minimum_energy_efficiency_ratio": 0.0,
+    "minimum_energy_efficiency_ratio_2": 0.0,
     "minimum_integrated_part_load_value": 0.0,
     "minimum_integrated_energy_efficiency_ratio": 0.0,
+    "minimum_combined_energy_efficiency_ratio": 0.0,
     "ptac_eer_coefficient_1": 0.0,
     "ptac_eer_coefficient_2": 0.0,
-    "minimum_scop": 0.0,
-    "cool_cap_ft": "",
-    "cool_cap_fflow": "",
-    "cool_eir_ft": "",
-    "cool_eir_fflow": "",
-    "cool_plf_fplr": "",
+    "off_mode_power": 0.0,
     "annotation": "",
 }
 
@@ -128,14 +136,14 @@ class HVACMinimumRequirementUnitaryAirConditioners(DBOperation):
     def validate_record_datatype(self, record):
         str_expected = [
             "template",
+            "equipment_type",
             "cooling_type",
+            "heating_type",
+            "subcategory",
+            "application",
+            "region",
             "start_date",
             "end_date",
-            "cool_cap_ft",
-            "cool_cap_fflow",
-            "cool_eir_ft",
-            "cool_eir_fflow",
-            "cool_plf_fplr",
         ]
 
         for f in str_expected:
@@ -145,15 +153,19 @@ class HVACMinimumRequirementUnitaryAirConditioners(DBOperation):
                 ), f"{f} requires to be a string, instead got {record[f]}"
 
         float_expected = [
+            "electric_power_phase",
             "minimum_capacity",
             "maximum_capacity",
             "minimum_seasonal_efficiency",
+            "minimum_seasonal_efficiency_2",
             "minimum_energy_efficiency_ratio",
+            "minimum_energy_efficiency_ratio_2",
             "minimum_integrated_part_load_value",
             "minimum_integrated_energy_efficiency_ratio",
+            "minimum_combined_energy_efficiency_ratio",
             "ptac_eer_coefficient_1",
             "ptac_eer_coefficient_2",
-            "minimum_scop",
+            "off_mode_power",
         ]
 
         for f in float_expected:
@@ -172,24 +184,26 @@ class HVACMinimumRequirementUnitaryAirConditioners(DBOperation):
 
         return (
             getattr_either("template", record),
+            getattr_either("equipment_type", record),
             getattr_either("cooling_type", record),
             getattr_either("heating_type", record),
             getattr_either("subcategory", record),
+            getattr_either("application", record),
+            getattr_either("electric_power_phase", record),
+            getattr_either("region", record),
             getattr_either("minimum_capacity", record),
             getattr_either("maximum_capacity", record),
             getattr_either("start_date", record),
             getattr_either("end_date", record),
             getattr_either("minimum_seasonal_efficiency", record),
+            getattr_either("minimum_seasonal_efficiency_2", record),
             getattr_either("minimum_energy_efficiency_ratio", record),
+            getattr_either("minimum_energy_efficiency_ratio_2", record),
             getattr_either("minimum_integrated_part_load_value", record),
             getattr_either("minimum_integrated_energy_efficiency_ratio", record),
+            getattr_either("minimum_combined_energy_efficiency_ratio", record),
             getattr_either("ptac_eer_coefficient_1", record),
             getattr_either("ptac_eer_coefficient_2", record),
-            getattr_either("minimum_scop", record),
-            getattr_either("cool_cap_ft", record),
-            getattr_either("cool_cap_fflow", record),
-            getattr_either("cool_eir_ft", record),
-            getattr_either("cool_eir_fflow", record),
-            getattr_either("cool_plf_fplr", record),
+            getattr_either("off_mode_power", record),
             getattr_either("annotation", record),
         )
