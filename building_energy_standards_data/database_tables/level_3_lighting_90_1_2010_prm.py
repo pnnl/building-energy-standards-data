46c3e8d5a9c54f2e76c310fb352c185713fea0e2
AmmarDeh <121573402+AmmarDeh@users.noreply.github.com>
2024-05-28 09:58:42 -0600

    Add 901 PRM lighting requirements (#52)
    * Expand 90.1 Lighting data
    
    Updated/expanded 90.1 lighting requirements
    
    * Removed Automotive general entry from level_2_lighting_space_types for 90.1-2022
    
    * Updated level_2_lighting_space_types.json
    
    Space type names are now the same across all code versions
    
    * Update level_3_lighting_90_1_definition table
    
    Updated the supplemental power density field from "supplemental_lighting_per_area" to "supplemental_lighting_power_density" for consistency.
    
    * updates field names for consistency
    
    updates field names to "lighting_power_density" for consistency.
    
    * Added 90.1 PRM lighting requirements
    
    Added 90.1 PRM lighting requirements
    
    * Minor updates to lighting requirements

---
+++ building_energy_standards_data/database_tables/level_3_lighting_90_1_2010_prm.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+from building_energy_standards_data.database_tables.level_3_lighting_90_1_prm_definition import (
+    LightDef901PRM,
+)
+
+TABLE_NAME = "level_3_lighting_90_1_2010_prm"
+
+
+class LightDef901PRM2010Table(LightDef901PRM):
+    def __init__(self):
+        super(LightDef901PRM2010Table, self).__init__(
+            table_name=TABLE_NAME,
+            initial_data_directory=f"building_energy_standards_data/database_files/{TABLE_NAME}",
+        )
