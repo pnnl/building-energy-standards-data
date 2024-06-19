c2535ac95cc1329f52fea1d1eb963591d29f0c6c
Lingzhe Wang <37538272+lzwang26@users.noreply.github.com>
2024-06-19 11:19:01 -0700

    Add IECC Lighting Data

---
+++ building_energy_standards_data/database_tables/level_3_lighting_IECC_2021.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+from building_energy_standards_data.database_tables.level_3_lighting_90_1_definition import (
+    LightDef901,
+)
+
+TABLE_NAME = "level_3_lighting_IECC_2021"
+
+
+class LightDefIECC2021Table(LightDef901):
+    def __init__(self):
+        super(LightDefIECC2021Table, self).__init__(
+            table_name=TABLE_NAME,
+            initial_data_directory=f"building_energy_standards_data/database_files/{TABLE_NAME}",
+        )
