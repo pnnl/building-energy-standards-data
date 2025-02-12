f564d6f19f3948cedb41587d4d2c8626b0e410a1
Lerond <jeremy.lerond@pnnl.gov>
2025-02-11 21:59:36 -0800

    Add 90.1-2022 thermal bridging requirements.

--- building_energy_standards_data/database_tables/__init__.py
+++ building_energy_standards_data/database_tables/__init__.py
@@ -90,6 +90,7 @@ __all__ = [
     "support_occupant_energy_behavior",
     "support_occupant_physical_characteristics",
     "envelope_thermal_bridging_requirements_IECC",
+    "envelope_thermal_bridging_requirements_90_1",
     # tables with foreign keys
     "level_2_lighting_space_types",
     "level_2_ventilation_space_types",
