73320d797a3b1972a1da15f507ee8ff3c4fa8ad6
Jeremy L. <5149279+lymereJ@users.noreply.github.com>
2025-02-17 15:07:14 -0800

    Update run.sh

--- osstd_data_update/run.sh
+++ osstd_data_update/run.sh
@@ -28,10 +28,10 @@ cd ../../
 rm -rf data_update
 echo "===="
 
-echo "Step 6: push new branch to OSSTD github (currently updating exterior lighting requirements (**/*ext_lgt*.json) data only)"
+echo "Step 6: push new branch to OSSTD github (currently updating exterior lighting requirements (**/*ext_ltg*.json) data only)"
 git config --global user.email "xuechen.lei@pnnl.gov"
 git config --global user.name "Xuechen (Jerry) Lei"
-git add **/*ext_lgt*.json
+git add **/*ext_ltg*.json
 #git add **/*energy_recovery*.json
 #git add **/*economizers*.json
 #git add **/*vrfs*.json
@@ -44,7 +44,7 @@ git add **/*ext_lgt*.json
 #git add **/*furnace*.json
 #git add **/*boiler*.json
 #git add **/*chiller*.json
-git commit -m "data_update (ext_lgt_only) ${COMMIT_ID:0:7}"
+git commit -m "data_update (ext_ltg_only) ${COMMIT_ID:0:7}"
 git remote set-url origin https://leijerry888:$GHTOKEN@github.com/NREL/openstudio-standards.git
 git push -u origin data_update_${COMMIT_ID:0:7}
 echo "===="
