003d9f788d4581680c1c9f11a419bb8e9322be69
Lerond <jeremy.lerond@pnnl.gov>
2025-03-26 09:54:10 -0700

    Apply Black formatting.

--- building_energy_standards_data/database_tables/hvac_minimum_requirements_computer_room_air_conditioners_189_1.py
+++ building_energy_standards_data/database_tables/hvac_minimum_requirements_computer_room_air_conditioners_189_1.py
@@ -11,7 +11,9 @@ class HVACMinimumRequirementComputerRoomAirConditioners1891Table(
     HVACMinimumRequirementComputerRoomAirConditioners
 ):
     def __init__(self):
-        super(HVACMinimumRequirementComputerRoomAirConditioners1891Table, self).__init__(
+        super(
+            HVACMinimumRequirementComputerRoomAirConditioners1891Table, self
+        ).__init__(
             table_name=TABLE_NAME,
             initial_data_directory=f"building_energy_standards_data/database_files/{TABLE_NAME}",
         )
