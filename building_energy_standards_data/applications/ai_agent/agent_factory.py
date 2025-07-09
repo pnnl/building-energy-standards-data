c16a1c0b3d68d753f4b7d1efcfc58db3e2d0077c
Slane-Holloway, Julian <julian.slane-holloway@pnnl.gov>
2025-07-09 11:30:20 -0700

    Add ai agent helper

---
+++ building_energy_standards_data/applications/ai_agent/agent_factory.py
@@ -0,0 +1,23 @@
+from langchain.agents import AgentExecutor
+from langchain.tools.base import BaseTool
+from langchain_core.language_models.base import BaseLanguageModel
+from langchain_community.utilities import SQLDatabase
+from typing import List, Optional
+from langchain_community.agent_toolkits import create_sql_agent
+
+def setup_besd_agent(
+    llm: BaseLanguageModel,
+    db_uri: str,
+    tools: Optional[List[BaseTool]] = [],
+    agent_type: str = "zero-shot-react-description",
+    verbose: bool = True,
+) -> AgentExecutor:
+    db = SQLDatabase.from_uri(db_uri)
+    sql_agent = create_sql_agent(
+        llm=llm,
+        db = db,
+        tools=tools,
+        agent=agent_type,
+        verbose=verbose
+    )
+    return sql_agent
