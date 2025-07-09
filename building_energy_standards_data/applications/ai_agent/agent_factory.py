db3cd5a1e119df5b1beb4e99c5a1d09ecc337bdc
Slane-Holloway, Julian <julian.slane-holloway@pnnl.gov>
2025-07-09 12:14:14 -0700

    Black formatting

--- building_energy_standards_data/applications/ai_agent/agent_factory.py
+++ building_energy_standards_data/applications/ai_agent/agent_factory.py
@@ -1,9 +1,11 @@
+from typing import List, Optional
+
 from langchain.agents import AgentExecutor
 from langchain.tools.base import BaseTool
 from langchain_core.language_models.base import BaseLanguageModel
 from langchain_community.utilities import SQLDatabase
-from typing import List, Optional
 from langchain_community.agent_toolkits import create_sql_agent
+
 
 def setup_besd_agent(
     llm: BaseLanguageModel,
@@ -14,10 +16,6 @@ def setup_besd_agent(
 ) -> AgentExecutor:
     db = SQLDatabase.from_uri(db_uri)
     sql_agent = create_sql_agent(
-        llm=llm,
-        db = db,
-        tools=tools,
-        agent=agent_type,
-        verbose=verbose
+        llm=llm, db=db, tools=tools, agent=agent_type, verbose=verbose
     )
     return sql_agent
